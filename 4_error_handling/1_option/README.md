9d18d42d8e6f2c08bac0e299745028c0d9fa5674
Mattsi <mattsi.jansky@codurance.com>
2023-01-12 19:51:17 +0000

    First error handling exercise

---
+++ 4_error_handling/1_option/README.md
@@ -0,0 +1,9 @@
+# Workshop: Option
+
+Use `cargo run` to run the project. Implement the unimplemented methods of `Inventory` to get the program to complete without errors.
+
+## Hints
+
+* `unimplemented!()` is just a function that panics when it is hit.
+* Remember that usize is a numeric type used for indexing collections
+* The `Vec` struct has a `remove` method that may be useful here
---
+++ 4_error_handling/1_option/src/main.rs
@@ -0,0 +1,42 @@
+enum Item {
+    HealthPotion(u8),
+    PoisonPotion(u8)
+}
+
+struct Inventory {
+    items: Vec<Item>,
+}
+
+impl Inventory {
+    fn new() -> Inventory {
+        unimplemented!();
+    }
+
+    fn push(&mut self, item: Item) {
+        unimplemented!();
+    }
+
+    fn get(&mut self, index: usize) -> Option<Item> {
+        unimplemented!();
+    }
+
+    fn len(&self) -> usize {
+       unimplemented!();
+    }
+}
+
+fn main() {
+    let mut player_inventory = Inventory::new();
+    player_inventory.push(Item::HealthPotion(10));
+    player_inventory.push(Item::PoisonPotion(5));
+
+    let health_potion = player_inventory.get(0).unwrap();
+    //Assert that health_potion is a health potion for 10 HP
+    assert!(matches!(health_potion, Item::HealthPotion(10)));
+    assert_eq!(1, player_inventory.len());
+
+    let poison_potion = player_inventory.get(0).unwrap();
+    //Assert that poison_potion is a poison potion for 5 HP
+    assert!(matches!(poison_potion, Item::PoisonPotion(5)));
+    assert_eq!(0, player_inventory.len());
+}
