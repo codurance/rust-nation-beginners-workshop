a20b08a7070d61681748431d2507d0dc7264681f
Mattsi <mattsi.jansky@codurance.com>
2023-02-12 16:44:28 +0000

    Complete 5_structs workshop

---
+++ 3_types/5_structs/README.md
@@ -0,0 +1,12 @@
+# Workshop: Enums
+
+Use `cargo run` to run the project. Without changing the code in the main function make changes to get the program to compile and run successfully (with a 0 exit status).
+
+There are seven test cases. After you get one passing, uncomment the next one until all seven are passing.
+
+## Hints
+
+* If you have a Rust plugin in your IDE it should be able to generate `use` statements (imports) automatically, eg by using alt+enter to get auto-complete suggestions.
+  * May not work if the struct is not public
+* As always listen to the compiler, it will provide helpful advice
+* 
---
+++ 3_types/5_structs/src/main.rs
@@ -0,0 +1,56 @@
+mod game {
+    pub struct Player {
+        name: String,
+        //…
+    }
+
+    pub struct InvulnerablePlayer {
+        name: String,
+        //…
+    }
+}
+
+fn main() {
+    //1 - Should construct a new Player object from name and health, and print debug info
+    let mut mario = Player::new("Mario", 100);
+    assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 100, items: [] }");
+
+    //2 - Should generate uppercase version of name
+    // assert_eq!(mario.name_uppercase(), String::from("MARIO"));
+
+    //3 - Should reduce health by specified amunt
+    // mario.take_damage(10);
+    // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [] }");
+
+    //4 - Should work with different values
+    // let mut luigi = Player::new("Luigi", 200);
+    // assert_eq!(format!("{:?}", luigi), "Player { name: \"Luigi\", health: 200, items: [] }");
+    // assert_eq!(luigi.name_uppercase(), String::from("LUIGI"));
+    // luigi.take_damage(100);
+    // assert_eq!(format!("{:?}", luigi), "Player { name: \"Luigi\", health: 100, items: [] }");
+
+    //5 - Should place picked up items in inventory
+    // assert_eq!(mario.no_of_items_carried(), 0);
+    // mario.pick_up_item("Super Star");
+    // assert_eq!(mario.no_of_items_carried(), 1);
+    // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [\"Super Star\"] }");
+
+    //6 - When eating a super star Mario should become invulnerable, and invalidate old player object
+    // by taking ownership of self in the `eat` method
+    // let invulnerable_mario = mario.eat();
+    // assert_eq!(format!("{:?}", invulnerable_mario), "InvulnerablePlayer { name: \"Mario\", health: 90, items: [] }");
+    // //Note that by consuming `mario` we now cannot access it. Because Mario cannot take damage after
+    // //eating a Super Star we do not implement the `take_damage` function in `InvulnerablePlayer`. This way
+    // //we can use Rust's type system to enforce an invariant. If anyone tries to apply damage to Mario
+    // //in his in invulnerable form the compiler will prevent that bug before it reaches run-time.
+    // //This is the power of Rust's type system and ownership!
+    // //Uncommenting the following line should cause a compile error:
+    // // let test = mario.name_uppercase();
+
+    //7 - given time has run out, invulnerable player becomes regular player, and invalidate
+    // //invulnerable player by taking ownership of self in `lose_invulnerability` method
+    // let mario = invulnerable_mario.lose_invulnerability();
+    // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [] }");
+    // //Uncommenting the following line should cause a compile error:
+    // // let test = invulnerable_mario;
+}
