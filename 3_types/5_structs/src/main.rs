59b8f7d2543d2b118981b7eb5a82ed78cbf79685
Mattsi-Jansky <34603584+Mattsi-Jansky@users.noreply.github.com>
2023-02-15 14:54:47 +0000

    Simplify the 5_structs exercise

--- 3_types/5_structs/src/main.rs
+++ 3_types/5_structs/src/main.rs
@@ -1,41 +1,38 @@
-mod game {
-    pub struct Player {
-        name: String,
-        //…
-    }
+pub struct Player {
+    name: String,
+    items: Vec<String>,
+    //…
+}
 
-    pub struct InvulnerablePlayer {
-        name: String,
-        //…
-    }
+pub struct InvulnerablePlayer {
+    name: String,
+    items: Vec<String>,
+    //…
 }
 
 fn main() {
     //1 - Should construct a new Player object from name and health, and print debug info
     let mut mario = Player::new("Mario", 100);
     assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 100, items: [] }");
-
-    //2 - Should generate uppercase version of name
-    // assert_eq!(mario.name_uppercase(), String::from("MARIO"));
-
-    //3 - Should reduce health by specified amount
+    
+    //2 - Should reduce health by specified amount
     // mario.take_damage(10);
     // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [] }");
 
-    //4 - Should work with different values
+    //3 - Should work with different values
     // let mut luigi = Player::new("Luigi", 200);
     // assert_eq!(format!("{:?}", luigi), "Player { name: \"Luigi\", health: 200, items: [] }");
     // assert_eq!(luigi.name_uppercase(), String::from("LUIGI"));
     // luigi.take_damage(100);
     // assert_eq!(format!("{:?}", luigi), "Player { name: \"Luigi\", health: 100, items: [] }");
 
-    //5 - Should place picked up items in inventory
+    //4 - Should place picked up items in inventory
     // assert_eq!(mario.no_of_items_carried(), 0);
     // mario.pick_up_item("Super Star");
     // assert_eq!(mario.no_of_items_carried(), 1);
     // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [\"Super Star\"] }");
 
-    //6 - When eating a super star Mario should become invulnerable, and invalidate old player object
+    //5 - When eating a super star Mario should become invulnerable, and invalidate old player object
     // by taking ownership of self in the `eat` method
     // let invulnerable_mario = mario.eat();
     // assert_eq!(format!("{:?}", invulnerable_mario), "InvulnerablePlayer { name: \"Mario\", health: 90, items: [] }");
@@ -47,7 +44,7 @@ fn main() {
     // //Uncommenting the following line should cause a compile error:
     // // let test = mario.name_uppercase();
 
-    //7 - given time has run out, invulnerable player becomes regular player, and invalidate
+    //6 - given time has run out, invulnerable player becomes regular player, and invalidate
     // //invulnerable player by taking ownership of self in `lose_invulnerability` method
     // let mario = invulnerable_mario.lose_invulnerability();
     // assert_eq!(format!("{:?}", mario), "Player { name: \"Mario\", health: 90, items: [] }");
